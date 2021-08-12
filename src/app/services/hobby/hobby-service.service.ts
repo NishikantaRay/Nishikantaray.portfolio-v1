431792a6c9afda387764546c81467e78edc5e883
Nishikantaray <nishikantaray1@gmail.com>
2021-08-12 23:14:38 +0530

    Update budget

--- src/app/services/hobby/hobby-service.service.ts
+++ src/app/services/hobby/hobby-service.service.ts
@@ -7,27 +7,27 @@ export class HobbyServiceService {
 getData(){
   const arrproject:any =[
     {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/sergio2.jpg",
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantarayportfolio/main/sergio2.jpg",
       "name":"Sergio"
     },
     {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/sergio.jpg",
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantarayportfolio/main/sergio.jpg",
       "name":"Sergio"
     },
     {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/face.jpg",
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantarayportfolio/main/face.jpg",
       "name":"Face Illustrator"
     },
     {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/img/sanjay.jpeg",
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantarayportfolio/main/img/sanjay.jpeg",
       "name":"Sanjay Dutt"
     },
     {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/img/carry.jpeg",
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantarayportfolio/main/img/carry.jpeg",
       "name":"CarryMinati"
     },
     {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/img/cards.jpeg",
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantarayportfolio/main/img/cards.jpeg",
       "name":"QueenofDecks"
     }
   ];
