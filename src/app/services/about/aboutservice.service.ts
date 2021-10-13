241935c3dc47c12c2badc375a668bc0be311aef9
Nishikantaray <nishikantaray1@gmail.com>
2021-10-14 00:46:45 +0530

    added meta tag

--- src/app/services/about/aboutservice.service.ts
+++ src/app/services/about/aboutservice.service.ts
@@ -12,7 +12,7 @@ export class AboutserviceService {
         name: 'About me',
         picname: 'Nishikanta Ray',
         intro:
-          'I am a second year Undergraduate student of Electronics and Communication Engineering at Silicon Institute of Technology,Bhubanewer,Odisha. I am a tech enthusiast who likes creating new things and experimenting with things. I am a fast learner and enjoy challenges encountered along the way. I am always willing to learn new things with full enthusiasm and passion.',
+          'I am a Third year Undergraduate student of Electronics and Communication Engineering at Silicon Institute of Technology,Bhubanewer,Odisha. I am a tech enthusiast who likes creating new things and experimenting with things. I am a fast learner and enjoy challenges encountered along the way. I am always willing to learn new things with full enthusiasm and passion.',
         url: 'https://avatars.githubusercontent.com/u/62615392?v=4',
       };
       return details;
